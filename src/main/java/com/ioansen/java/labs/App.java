cf762d69d76fb59de7a00d0e30d0fffc6b2e6917
ioansen <ioan.sndr@gmail.com>
2018-10-06 18:34:37 +0300

    added project

---
+++ src/main/java/com/ioansen/java/labs/App.java
@@ -0,0 +1,13 @@
+package com.ioansen.java.labs;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
